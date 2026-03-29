a02c2ee11b99c5fedebcdcf1708bacc47a54dd45
l-bonfim <l_bonfim@hotmail.com>
2026-03-29 00:11:55 -0300

    testando random value no raylib

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@ int main(){
 
     // Texture2D alzaratest = LoadTexture("../assets/alzara-img-noedit.png");
     SetTargetFPS(60);
+    int numero = GetRandomValue(0, 100);
+    char num[3];
+    sprintf(num, "%d", numero);
 
 
     while (!WindowShouldClose())
@@ -24,6 +27,7 @@ int main(){
         ClearBackground(WHITE);
         // DrawTexture(alzaratest, 0, 0, WHITE);
         DrawText("testando imagem", 300, 300, 15, RED);
+        DrawText(num, 100, 100, 30, RED);
         
 
         
