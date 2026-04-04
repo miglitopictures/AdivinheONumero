16d8505b75a067f7520781e7fd3d300d35ccd1d5
Miguel Duarte <miguelmddesign@gmail.com>
2026-04-04 16:18:05 -0300

    reestruturando o projeto a partir das intrucoes de renan

--- build_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Compile
-clang src/*.c -o game -Iinclude -Llib/mac -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework OpenGL
--- build_win.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Compile for Windows
-gcc src/*.c -o game.exe -Iinclude -Llib/windows -lraylib -lopengl32 -lgdi32 -lwinmm
